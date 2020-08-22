e9c33738c2fa4fcde893a4274a21cb1127496d66
Sota Funabashi <sfunabashi276@gmail.com>
2020-08-22 11:52:53 +0900

    add demo

---
+++ Assets/MonoStreamPackage/Demo/Mgr.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mono.Demo
+{
+    public class Mgr : MonoBehaviour
+    {
+        private void Awake()
+        {
+            MonoStream.Subscribe(0.5f, () => Debug.Log("HI"));
+            MonoStream.Subscribe(0.5f, () => Debug.Log("HI"));
+            MonoStream.Subscribe(0.5f, () => Debug.Log("HI"));
+            MonoStream.Subscribe(1f, () => Debug.Log("HI"));
+            MonoStream.Subscribe(2f, () => Debug.Log("HI"));
+        }
+    }
+}
---
+++ Assets/MonoStreamPackage/Scripts/StreamObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace Mono
+{
+    public struct StreamObject
+    {
+        public readonly float Delay;
+        public readonly Action OnSubscribed;
+
+        public StreamObject(float delay, Action onSubscribed)
+        {
+            Delay = delay;
+            OnSubscribed = onSubscribed;
+        }
+    }
+}
